c92052c92887d531619609d07267342e8d6c4455
PAPATIRI <syariftaufik11@gmail.com>
2023-03-11 19:25:34 +0700

    fix minor

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('dashboard',[\App\Http\Controllers\Dashboard::class,'index'])->name('
 Route::get('data-toko',[\App\Http\Controllers\TokoController::class,'index'])->name('toko.listToko');
 Route::get('tambah-toko',[\App\Http\Controllers\TokoController::class,'create'])->name('toko.tambahToko');
 Route::post('simpan-toko',[\App\Http\Controllers\TokoController::class,'store'])->name('toko.simpanToko');
-Route::get('list-toko',[\App\Http\Controllers\TokoController::class,'listToko'])->name('toko.DaftarToko');
+Route::get('list-toko',[\App\Http\Controllers\TokoController::class,'listToko'])->name('list-toko');
 Route::delete('hapus-toko/{id}',[\App\Http\Controllers\TokoController::class,'destroy'])->name('toko.hapusToko');
 Route::get('detail-toko/{id}',[\App\Http\Controllers\TokoController::class,'detailToko'])->name('toko.detailToko');
 
